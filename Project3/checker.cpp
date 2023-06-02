aac2ce89c602aaa9f2a093fb4cc725af8774f9d8
jinwoo <jori3597@naver.com>
2023-06-02 13:58:18 +0900

    checker class 수정

--- Project3/checker.cpp
+++ Project3/checker.cpp
@@ -1,22 +1,45 @@
 #include<string>
+#include<cstdlib>
 using namespace std;
 
 class Checker
 {
 public:
+
 	int getStringLength(string firstStr, string secondStr)
+	{
+		if(firstStr.length() == secondStr.length())
+		{
+			return 60;
+		}
+		else if(firstStr.length() > secondStr.length())
+		{
+			if (firstStr.length() / secondStr.length() >= 2) return 0;
+		}
+		else if (firstStr.length() <  secondStr.length())
+		{
+			if (secondStr.length() / firstStr.length() >=  2) return 0;
+		}
+		else
+		{
+			return checkPartialPoint(firstStr.length(), secondStr.length());
+		}
+	}
+
+	int checkPartialPoint(int firLength, int secondLength)
 	{
 		int gapLength = 0;
 		int minLength;
 
-		if (firstStr.length() >= secondStr.length()) {
-			gapLength = firstStr.length() - secondStr.length();
-			minLength = secondStr.length();
+		if (firLength >= secondLength) 
+		{
+			gapLength = firLength - seco ndLength;
+			minLength = secondLength;
 		}
 		else
 		{
-			gapLength = secondStr.length() - firstStr.length();
-			minLength = firstStr.length();
+			gapLength = secondLength - firLength;
+			minLength = firLength;
 		}
 
 		return (1 - gapLength / minLength) * 60;
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,9 +1,18 @@
 #include "pch.h"
 #include"../Project3/Checker.cpp"
 
-TEST(TestCaseName, CheckLength) {
+TEST(TestCaseName, CheckLength1) {
   EXPECT_EQ(60, Checker().getStringLength("ASD", "DSA"));
-  EXPECT_EQ(0, Checker().getStringLength("A", "BB"));
-  EXPECT_EQ(60, Checker().getStringLength("AAABB", "BAA"));
-  EXPECT_EQ(60, Checker().getStringLength("AA", "AAA"));
+}
+
+TEST(TestCaseName, CheckLength2) {
+	EXPECT_EQ(0, Checker().getStringLength("A", "BB"));
+}
+
+TEST(TestCaseName, CheckLength3) {
+	EXPECT_EQ(53, Checker().getStringLength("AAABB", "BAA"));
+}
+
+TEST(TestCaseName, CheckLength4) {
+	EXPECT_EQ(53, Checker().getStringLength("AA", "AAA"));
 }
